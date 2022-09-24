ef2069a1eeaf5bcf5040f1ad1679217cd45939f2
David Herring <davidherring@Davids-MBP.home>
2022-09-24 02:16:00 -0500

    adding joke function and adding compatibility api

--- assetsRandom/random.js
+++ assetsRandom/random.js
@@ -1,8 +1,20 @@
+// joke api 
+const setUpLine = document.getElementById('setup')
+const responseLine = document.getElementById('punch')
 
+// love api 
+const yName = document.getElementById('userName').value;
+const lName = document.getElementById('loverName').value[0];
+const percentage = document.getElementById('percentage');
+const result = document.getElementById('result');
+
+
+// random joke api call
 const jokeBtn = document.getElementById('btn-one');
 jokeBtn.addEventListener('click', jokesApi)
 
 function jokesApi () {
+	
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,13 +22,42 @@ const options = {
 		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
 	}
 };
-
 fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
 	.then(response => response.json())
 	.then(response => {
-        console.log(response.body[0].setup + " = " + response.body[0].punchline)
+		const setUp = response.body[0].setup;
+		const punch = response.body[0].punchline;
+		setUpLine.append(setUp);
+		responseLine.append(punch)
+		responseLine.style.fontSize='1rem';
+		responseLine.style.fontWeight='bold';
         return response;
-    }
-    )
-    JokesOutput();
+    })
+}
+
+const loveBtn = document.getElementById('btn-two')
+loveBtn.addEventListener('click', loveApi)
+
+function loveApi () {
+
+
+// get love compatibility api 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '00e3db074cmsh859ff928fa0e703p114d09jsn7ee010402a05',
+		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+	}
+};
+fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', options)
+	.then(response => response.json())
+	.then(response =>  {
+		const getPercentage = response.percentage;
+		const getResults = response.result;
+		percentage.append(`${getPercentage}%`);
+		result.append(getResults);
+		console.log(response);
+		console.log(yName)
+		console.log(lName)
+	})
 }
